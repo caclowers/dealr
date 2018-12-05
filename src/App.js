77fb1e62cfbbd62968fbf033584e8a7012717854
Casey <caclowers@gmail.com>
2018-12-05 08:21:25 -0600

    added sweetalert//added server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+import SubmitPopup from './SubmitPopup';
+
+const MySwal = withReactContent(swal);
+ let scraperjs = require('scraperjs');
+  scraperjs.StaticScraper.create('https://news.ycombinator.com/')
+      .scrape(function($) {
+          return $(".title a").map(function() {
+              return $(this).text();
+          }).get();
+      })
+      .then(function(news) {
+          console.log(news);
+      })
 
 class App extends Component {
+
+ 
+
+submit = (event) => {
+    console.log('hi');
+    event.preventDefault();
+    MySwal.fire({
+      html: <SubmitPopup />,
+      showCloseButton: true,
+      showCancelButton: false,
+      focusConfirm: true,
+      confirmButtonText:
+        '<i class="fa fa-thumbs-up"></i> OK',
+      confirmButtonAriaLabel: 'Thumbs up, great!',
+      cancelButtonText:
+        '<i class="fa fa-thumbs-down">Close</i>',
+      cancelButtonAriaLabel: 'Thumbs down',
+    })
+  
+  }
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <h1>$DEALR</h1>
+        <form>
+          <input placeholder="save money on..."
+          style={{width:250}}>
+          </input>
+          <button onClick={this.submit}>search</button>
+        </form>
         </header>
+        
       </div>
     );
   }
---
+++ src/SubmitPopup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SubmitPopup.css';
+
+export default ({ style }) => (
+  <div id="swalComponent" >
+    
+    <h2>
+     Watch Your Inbox!
+    </h2>
+   
+    
+  </div>
+);
